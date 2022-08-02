9396a70e5940cf05de27803c50f1131596565922
liberaldev <ssharp0614@gmail.com>
2022-08-02 20:26:09 +0900

    설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 // vite.config.js
 import {sveltekit} from '@sveltejs/kit/vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import { pwaConfiguration } from './pwa-configuration.js';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit(), VitePWA()],
+  plugins: [sveltekit(), VitePWA(pwaConfiguration)],
 };
 
 export default config;
